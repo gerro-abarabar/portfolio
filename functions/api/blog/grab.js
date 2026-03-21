bd111443c0fd09e8fb48e08de9b8a093a93d106c
Gerro Abarabar <gerrolabarabar@gmail.com>
2026-03-21 14:24:38 +0800

    debugging problem with cloudflare

--- functions/api/blog/grab.js
+++ functions/api/blog/grab.js
@@ -1,9 +1,10 @@
 export async function onRequestPost(context){
     const res = await context.request.json();
     const { amount } = res; // The amount of posts to fetch, e.g., 5 for the latest 5 posts
-
+    console.log("Grabbing posts",res);
     // const data = await context.env.beronicous_db.prepare('SELECT * FROM blog_posts ORDER BY id DESC LIMIT ?').all(amount);
     const data = await context.env.beronicous_db.prepare('SELECT * FROM blog_posts').all(); // Temporary until posts are more than 5
+    console.log("We got: ",data.results)
     return new Response(JSON.stringify(data.results), {
     headers: { 'Content-Type': 'application/json' },
     });
